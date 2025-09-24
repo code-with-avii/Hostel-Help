e68f75282c8f2a458f063bb0e420e796bedd9d4f
code-with-avii <aviimondal689@gmail.com>
2025-09-25 03:41:50 +0530

    changed

--- models/UserProfile.js
+++ models/UserProfile.js
@@ -1,13 +1,13 @@
 import mongoose from 'mongoose';
 
 const userProfileSchema = new mongoose.Schema({
-    email: { type: String, required: true, trim: true, lowercase: true, unique: true },
+    email: { type: String, required: true, trim: true, lowercase: true },
     year: { type: String, default: '' },
     department: { type: String, default: '' },
     number: { type: String, default: '' }
 }, { timestamps: true });
 
-userProfileSchema.index({ email: 1 }, { unique: true });
+userProfileSchema.index({ email: 1 });
 
 const UserProfile = mongoose.model('UserProfile', userProfileSchema);
 
